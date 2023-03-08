bec7a8fe714e5aac481137ff172cb68227f8a639
Garbaya Meriem <garbaya.mariem@gmail.com>
2023-03-08 15:42:21 +0100

    fifth commit

---
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -0,0 +1,12 @@
+package com.hemebiotech.analytics;
+
+public class AnalyticsCounter {
+
+	public static void main(String[] args) throws Exception {
+
+		WriteSymptomData finalResult = new WriteSymptomData();
+		finalResult.WriteFileData();
+
+	}
+
+}
